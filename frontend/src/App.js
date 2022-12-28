e482e4557b7457bfb20420ab5c2b8220d2fef0a1
ehoneahobed <ehoneahobed@hotmail.com>
2022-12-28 23:10:35 +0000

    create app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Header from "./Components/header/Header";
+import Notes from "./Components/notes/Notes";
+import AddNote from "./Components/addnote/AddNote";
+import NoteDetails from "./Components/notedetails/NoteDetails";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router className="App">
+			<Header />
+			<section className="App__notes">
+				<Routes>
+					<Route path="/" element= {<Notes />} />
+					<Route path="/addnote" element= {<AddNote />} />
+					<Route path="/note/:noteId" element= {<NoteDetails />} />
+				</Routes>
+			</section>
+		</Router>
+	);
 }
 
 export default App;
