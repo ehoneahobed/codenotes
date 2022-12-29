763e251a4eff83916250dda29641261d04bf2d8c
ehoneahobed <ehoneahobed@hotmail.com>
2022-12-29 01:24:17 +0000

    Add hamburger menu for smaller screens

--- frontend/src/Components/header/Header.js
+++ frontend/src/Components/header/Header.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./header.css"
+import "./header.css";
 
 function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header__wrapper">
-	  <Link to="/" className="link"> <h1 className="header__title">CodeNotes</h1> </Link>
-	  <div className="header__links">
-		<ul>
-			<li><Link to="/" className="link">All Notes</Link></li>
-			<li><Link to="/addnote" className="link">Add Note</Link></li>
-		</ul>
-	  </div>
-	  </div>
+        <Link to="/" className="link">
+          {" "}
+          <h1 className="header__title">CodeNotes</h1>{" "}
+        </Link>
+        <div className="header__links">
+          <i class={`uil uil-bars ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}></i>
+          <ul className={`header__links-ul ${isOpen && "open"}`}>
+            <li>
+              <Link to="/" className="link" onClick={() => setIsOpen(!isOpen)}>
+                All Notes
+              </Link>
+            </li>
+            <li>
+              <Link to="/addnote" className={`link ${isOpen && "open"}`}  onClick={() => setIsOpen(!isOpen)}>
+                Add Note
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
